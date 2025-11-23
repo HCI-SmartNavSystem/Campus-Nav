31a2fc87a7fb4b4f71184f52fd749607aa8335ea
keajoker <119678212+keajoker@users.noreply.github.com>
2025-11-23 18:27:47 +0400

    V-1.0

--- js/pwa.js
+++ js/pwa.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./service-worker.js")   // 👈 IMPORTANT: "./", not "/"
+      .register("./service-worker.js")   // 👈 NOTE: "./", not "/"
       .then(reg => console.log("SW registered", reg))
       .catch(err => console.error("SW registration failed", err));
   });
